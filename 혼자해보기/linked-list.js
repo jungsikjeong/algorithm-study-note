a3537b02ce494dff94595d979e5a2ea4a83ce134
jungsikjeong <wndtlr1024@gmail.com>
2025-12-13 21:08:19 +0900

    링크드리스트 혼자해본거 + 지피티 도움받으면서 풀어본거 (다시해볼예정)

--- 혼자해보기/linked-list.js
+++ 혼자해보기/linked-list.js
@@ -7,70 +7,63 @@ class Node {
 
 class LinkedList {
     constructor(value){
-        this.head ={
-            value :value,
-            next :null
-        }
-
-        this.tail=this.head
-        this.length = 1;
+        this.head ={ value,next:null }
+        this.tail= this.head
+        this.length= 1;
     }
 
     prepend(value){
-        const newNode = new Node(value);
-        newNode.next = this.head;
-        this.head = newNode;
+        const newNode = new Node(value)
+        newNode.next = this.head
+        this.head=newNode
         this.length++;
     }
     
 
     append(value){
-        const newNode = new Node(value);
-        this.tail.next= newNode;
-        this.tail = newNode;
-        this.length++
+       const newNode = new Node(value)
+       this.tail.next = newNode
+       this.tail= newNode
+       this.length++
+       
     }
 
     findNode(index){
-        let count = 0;
-        let currentNode = this.head
+        let count = 0
+        let currentNode;
         while(count !== index){
-        currentNode = currentNode.next
-           count++;
+            currentNode = this.head
+            count++
         }
-        return currentNode;
+        return currentNode
     }
 
     insert(index,value){
-        if(index ===0){
-            this.prepend(value)
-            return
-        }
-        
-        if(index >= this.length){
-            this.append(value)
-            return
-        }
+      if(index===0){
+        this.prepend(value);
+        return 
+      }
 
-    const newNode = new Node(value)
-    const prevNode = this.findNode(index - 1)
-    const nextNode = prevNode.next
-    newNode.next = nextNode; // 2번 역할
-    prevNode.next = newNode; // 3번 역할 
-    this.length++;
+      if(index >= this.length){
+        this.append(value)
+        return
+      }
+
+      const newNode = new Node(value);
+      const prevNode = this.findNode(index - 1 )
+      const nextNode = prevNode.next;
+      newNode.next = nextNode; // 2번 역할
+      prevNode.next = newNode; // 3번 역할
+      this.length++;
+
     }
 
     remove(index){
-        const prevNode = this.findNode(index - 1);
-        const removeNode = prevNode.next;
-        prevNode.next = removeNode.next
-        this.length--;
+      const prevNode = this.findNode(index - 1)
+      const removeNode = prevNode.next
+      prevNode.next = removeNode.next
+      this.length--;
     }
 }
 
 
-/**
- * prevNode.next = removeNode.next; 이 줄이 연결을 "끊고 건너뛰는" 핵심임
-삭제할 노드는 메모리상엔 남아있지만, 리스트에서 더 이상 연결되지 않으니 "고아가 된 것처럼 버려져서" GC(Garbage Collector)가 정리하게 됨
- */
-remove(index) 
