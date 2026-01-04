88b0eca0ca2225c7f0b332761a199f870ea32fda
jungsikjeong <wndtlr1024@gmail.com>
2026-01-04 21:54:25 +0900

    연결리스트 큐 다시해본거( 주석포함 )

--- 혼자해보기/연결리스트큐2.js
+++ 혼자해보기/연결리스트큐2.js
@@ -1,11 +1,11 @@
 class Node {
   constructor(value) {
-    this.value = value;
-    this.next = null;
+    this.first = valie;
+    this.last = null;
   }
 }
 
-class Queue {
+class queue {
   constructor(value) {
     this.first = null;
     this.last = null;
@@ -13,32 +13,41 @@ class Queue {
   }
 
   enqueue(value) {
+    // 새 노드를 만든다.
     const newNode = new Node(value);
-
+    // 비어있다면? first와 last 둘 다 새 노드로 지정.
     if (this.length === 0) {
       this.first = newNode;
       this.last = newNode;
     } else {
+      // 내용물이 있다면?
       const oldLast = this.last;
       oldLast.next = newNode;
       this.last = newNode;
     }
+    // 길이를 1 늘린다.
     this.length++;
   }
 
   dequeue() {
+    // 비어있는지 확인 (비었으면 null 반환).
     if (!this.first) return null;
 
+    // 나갈 노드(first)를 변수에 저장한다.
     const removedNode = this.first;
 
-    if (this.first === this.last) {
+    // 하나뿐이라면? first와 last 둘 다 null로 만든다.
+    if (this.length === 1) {
       this.first = null;
       this.last = null;
     } else {
-      this.first = this.first.next; // TODO:이부분 이해를 했는데, 다시 풀어볼 예정
+      // 여러 개라면?
+      this.first = this.first.next;
     }
 
+    // 길이를 1 줄인다.
     this.length--;
+    // 나간 노드의 값을 반환한다.
     return removedNode;
   }
 }
